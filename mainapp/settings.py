98d75f6f2a0c64df0a332fe7f4630dc1757e5b2f
cecep31 <cecepjanuardi31@gmail.com>
2021-03-13 17:50:09 +0700

    fix problem migration

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mainapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['TEMPLATES'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+LOGIN_REDIRECT_URL= 'show'
+LOGIN_URL='login'
