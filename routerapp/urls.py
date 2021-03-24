03fd3b63dec5b6bd9e3594605340667ab2f59c28
cecep31 <cecepjanuardi31@gmail.com>
2021-03-24 17:07:54 +0700

    n

--- routerapp/urls.py
+++ routerapp/urls.py
@@ -1,3 +1,5 @@
+from routerapp import forms
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView,LogoutView
 from django.forms import widgets
 from django.urls import path
